450df8f03145c3580e083052014b606de9116066
theusazevedd <matheus_ma02@hotmail.com>
2026-04-24 23:35:53 -0300

    adicionado esqueleto do projeto

---
+++ task-bff/src/main/java/com/azevedo/task_bff/TaskBffApplication.java
@@ -0,0 +1,13 @@
+package com.azevedo.task_bff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskBffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskBffApplication.class, args);
+	}
+
+}
---
+++ task-bff/src/test/java/com/azevedo/task_bff/TaskBffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azevedo.task_bff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskBffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
